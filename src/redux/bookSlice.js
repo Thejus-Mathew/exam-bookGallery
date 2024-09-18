84b1bfbbe3d6832aa3417c970dbc9d54e77a9aa6
I-DUDE-I <thejusmathew8888@gmail.com>
2024-09-18 19:38:21 +0530

    xcg

--- src/redux/bookSlice.js
+++ src/redux/bookSlice.js
@@ -6,11 +6,17 @@ const bookSlice = createSlice({
     initialState:[],
     reducers:{
         addBook:(state,action)=>{
-            state.push(action.payload)
+            if(state.length>=1){
+                const id = state[state.length-1].id+1
+                state.push(action.payload)
+                state[state.length-1].id = id
+            }else{
+                state.push(action.payload)
+                state[0].id = 1
+            }
         },
         deleteBook:(state,action)=>{
-            const index = state.indexOf(action.payload)
-            state.splice(index,1)
+            return state.filter(item=>item.id != action.payload.id)
         }
     }
 })
